d187dbc73c787933fa7d6de59032976198e8f02f
Grant Jackson <Grant.Jackson@uk.cf247.net>
2017-09-06 14:10:44 +0100

    now i've added the transition, sign agreement and agreementCompleted pages

---
+++ src/app/transitionPage/transition.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'transition',
+    templateUrl: 'transition.component.html'
+})
+
+export class TransitionComponent {
+    loadingGifUrl = 'assets/transition.gif';
+
+}
