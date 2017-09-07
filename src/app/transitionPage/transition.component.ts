f8c4a64252ff0b68fd441030b1c74ab745da8899
Steve Baker <ste.baker.92@gmail.com>
2017-09-07 10:04:16 +0100

    clear timer on ngDestroy

--- src/app/transitionPage/transition.component.ts
+++ src/app/transitionPage/transition.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 import { Observable } from 'rxjs/Rx';
 
 @Component({
@@ -7,15 +8,20 @@ import { Observable } from 'rxjs/Rx';
     styleUrls: ['transition.component.css']
 })
 
-export class TransitionComponent implements OnInit {
+export class TransitionComponent implements OnInit, OnDestroy {
     loadingGifUrl = 'assets/transition.gif';
 
+    timeout: any; //NodeJS.Timer;
+
     ngOnInit() {
-        setTimeout(() =>
-            this.changePage(), 5000);
+        this.timeout = setTimeout(() => this.changePage(), 5000);
     }
 
     changePage(): void {
         location.href = '/home';
+    }
+
+    ngOnDestroy() {
+        clearTimeout(this.timeout);
     }
 }
