c141dcd6986746bde352e725a5acacca6328c4d8
Steve Baker <ste.baker.92@gmail.com>
2017-09-07 15:41:10 +0100

    Changed panel delay

--- src/app/signAgreement/signAgreement.component.ts
+++ src/app/signAgreement/signAgreement.component.ts
@@ -2,44 +2,46 @@ import { Component } from '@angular/core';
 import { LoremIpsum } from 'app/signAgreement/loremIpsum';
 
 @Component({
-    selector: 'sign-agreement',
-    templateUrl: 'signAgreement.component.html',
-    styleUrls: ['./signAgreement.style.css']
-  })
+  selector: 'sign-agreement',
+  templateUrl: 'signAgreement.component.html',
+  styleUrls: ['./signAgreement.style.css']
+})
 
-  export class SignAgreementComponent {
-    agreementBody = LoremIpsum.text;
-    completeButtonEnabled = false;
-    SECCIagreementComplete = false;
-    TnCsagreementComplete = false;
-    HPagreementComplete = false;
-    DDMagreementComplete = false;
-    timeout: any; // NodeJS.Timer;
-    accordion: any;
+export class SignAgreementComponent {
+  agreementBody = LoremIpsum.text;
+  completeButtonEnabled = false;
+  SECCIagreementComplete = false;
+  TnCsagreementComplete = false;
+  HPagreementComplete = false;
+  DDMagreementComplete = false;
+  timeout: any; // NodeJS.Timer;
+  accordion: any;
 
-    complete(): void {
-      if (this.completeButtonEnabled) {
-        location.href = '/completeAgreement';
-      }
+  panelDelay = 750;
+
+  complete(): void {
+    if (this.completeButtonEnabled) {
+      location.href = '/completeAgreement';
     }
+  }
 
-    setSECCIagreement(val, acc): void {
-      this.SECCIagreementComplete = val;
-      this.timeout = setTimeout(() => acc.toggle('tncPanel'), 500);
-    }
+  setSECCIagreement(val, acc): void {
+    this.SECCIagreementComplete = val;
+    this.timeout = setTimeout(() => acc.toggle('tncPanel'), this.panelDelay);
+  }
 
-    setTnCsagreement(val, acc): void {
-      this.TnCsagreementComplete = val;
-      this.timeout = setTimeout(() => acc.toggle('hpPanel'), 500);
-    }
+  setTnCsagreement(val, acc): void {
+    this.TnCsagreementComplete = val;
+    this.timeout = setTimeout(() => acc.toggle('hpPanel'), this.panelDelay);
+  }
 
-    setHPagreement(val, acc): void {
-      this.HPagreementComplete = val;
-      this.timeout = setTimeout(() => acc.toggle('ddmPanel'), 500);
-    }
+  setHPagreement(val, acc): void {
+    this.HPagreementComplete = val;
+    this.timeout = setTimeout(() => acc.toggle('ddmPanel'), this.panelDelay);
+  }
 
-    setDDMagreement(val): void {
-      this.DDMagreementComplete = val;
-      this.completeButtonEnabled = val;
-    }
+  setDDMagreement(val): void {
+    this.DDMagreementComplete = val;
+    this.completeButtonEnabled = val;
+  }
 }
